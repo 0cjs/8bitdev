30258443fcd86d179d6f14772ebba9cb8b1df669
Curt J. Sampson <cjs@cynic.net>
2019-09-11 06:30:59 +0900

    src/simple_test: Extract Test MPU class for testing framework

--- src/simple_test.py
+++ src/simple_test.py
@@ -5,32 +5,77 @@ LDXz    = 0xA6      # zero page
 NOP     = 0xEA
 
 ####################################################################
-#   Utility routines
+#   Test framework
 
-#   These functions should probably be replaced by a class
-#   encapsulating the MPU with these functions on it.
+class TMPU():
 
-def assertregs(mpu, pc=None, a=None, x=None, y=None, sp=None, p=None):
-    ''' Assert any registers specified have given values.
+    def __init__(self):
+        self.mpu = MPU()
 
-        This needs to be tweaked to produce better output on failure.
-        - Hex comparisions would be nice.
-        - Can we end the stack trace one level above this?
-    '''
-    if pc is not None:  assert pc == mpu.pc
-    if  a is not None:  assert  a == mpu.a
-    if  x is not None:  assert  x == mpu.x
-    if  y is not None:  assert  y == mpu.y
-    if sp is not None:  assert sp == mpu.sp
-    if  p is not None:  assert  p == mpu.p
+    def setregs(self, pc=None, a=None, x=None, y=None, sp=None):
+        m = self.mpu
+        if pc is not None:  m.pc = pc
+        if  a is not None:   m.a =  a
+        if  x is not None:   m.x =  x
+        if  y is not None:   m.y =  y
+        if sp is not None:  m.sp = sp
+        #   We don't do processor status register here as flags should
+        #   be set/reset individually, particularly because we should
+        #   avoid ever changing unused bits 5 and 6.
 
-def deposit(mem, addr, values):
-    mem[addr:addr+len(values)] = values
+    def assertregs(self, pc=None, a=None, x=None, y=None, sp=None, p=None):
+        ''' Assert any registers specified have given values.
 
-def test_deposit():
-    mem =  [0,0,0,0,0,0,0,0,0,0]
-    deposit(mem, 2, [9, 2, 3, 8])
-    assert [0,0,9,2,3,8,0,0,0,0] == mem
+            This needs to be tweaked to produce better output on failure.
+            - Hex comparisions would be nice.
+            - Can we end the stack trace one level above this?
+        '''
+        m = self.mpu
+        if pc is not None:  assert pc == m.pc
+        if  a is not None:  assert  a == m.a
+        if  x is not None:  assert  x == m.x
+        if  y is not None:  assert  y == m.y
+        if sp is not None:  assert sp == m.sp
+        if  p is not None:
+            #   Ignore unused bits 5 and 4 in the processor status register.
+            assert  p & 0b11001111 == m.p & 0b11001111
+
+    def deposit(self, addr, values):
+        self.mpu.memory[addr:addr+len(values)] = values
+
+    def step(self):
+        self.mpu.step()
+
+
+def test_testmpu():
+    tmpu = TMPU()
+    assert [0]*0x10000 == tmpu.mpu.memory
+
+def test_deposit_list():
+    tmpu = TMPU()
+    tmpu.deposit(6, [9, 2, 3, 8])
+    assert [0]*6 + [9, 2, 3, 8] + [0]*(0x10000 - 6 - 4) == tmpu.mpu.memory
+
+def test_regs():
+    tmpu = TMPU()
+    tmpu.assertregs(0, 0, 0, 0, 0xff)
+
+    tmpu.setregs(y=4, a=2)
+    tmpu.assertregs(y=4, a=2)
+    tmpu.assertregs(0000, 2, 0, 4, 0xff)
+
+    tmpu.setregs(1, 3, 5, 7, 0x80)
+    tmpu.assertregs(1, 3, 5, 7, 0x80)
+
+    #   We should never compare P status register bits 5 and 4.
+    #   Some of these values for P are probably invalid in this
+    #   emulator, but we never run in these states.
+    tmpu.assertregs(p=0b00110000); tmpu.assertregs(p=0)
+    tmpu.mpu.p = 0
+    tmpu.assertregs(p=0b00110000); tmpu.assertregs(p=0)
+    tmpu.mpu.p = 0xff
+    tmpu.assertregs(p=0b11001111); tmpu.assertregs(p=0xff)
+
 
 ####################################################################
 #   Tests
@@ -40,20 +85,20 @@ def test_mpu_step():
         that we're using the MPU API correctly.
     '''
     #   See py65/monitor.py for examples of how to set up and use the MPU.
-    mpu = MPU()
+    tmpu = TMPU()
 
-    mpu.memory[7] = 0x7E
-    deposit(mpu.memory, 0x400, [
+    tmpu.deposit(7, [0x7E])
+    tmpu.deposit(0x400, [
         LDAi, 0xEE,
         LDXz, 0x07,
         NOP,
     ])
-    assert   0x07 == mpu.ByteAt(0x403)
-    assert 0xEEA9 == mpu.WordAt(0x400)  # LSB, MSB
+    assert   0x07 == tmpu.mpu.ByteAt(0x403)
+    assert 0xEEA9 == tmpu.mpu.WordAt(0x400)  # LSB, MSB
 
-    assertregs(mpu, 0, 0, 0, 0)
-    mpu.pc = 0x400
+    tmpu.assertregs(0, 0, 0, 0)
+    tmpu.setregs(pc=0x400)
 
-    mpu.step(); assertregs(mpu, 0x402, a=0xEE)
-    mpu.step(); assertregs(mpu, 0x404, x=0x7E)
-    mpu.step(); assertregs(mpu, 0x405, 0xEE, 0x7E, 0x00)
+    tmpu.step(); tmpu.assertregs(0x402, a=0xEE)
+    tmpu.step(); tmpu.assertregs(0x404, x=0x7E)
+    tmpu.step(); tmpu.assertregs(0x405, 0xEE, 0x7E, 0x00)

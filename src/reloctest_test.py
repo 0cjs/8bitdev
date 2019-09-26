e8e1a666ecbccad522dc1098c94af305802d9805
Curt J. Sampson <cjs@cynic.net>
2019-09-26 16:47:19 +0900

    src/reloctest*: Show that relocatable global symbols also have wrong address
    This adds a global symbol to reloctest.a65 and demonstrates (with
    an xfailed test) that, just like local symbols, does not have its
    relocated address in the linker listing symbol table.

--- src/reloctest_test.py
+++ src/reloctest_test.py
@@ -3,7 +3,15 @@ import  pytest
 
 @pytest.fixture
 def M():
-    return Machine()
+    M = Machine()
+    M.load('.build/obj/reloctest')
+    return M
+
+@pytest.mark.xfail(strict=True)
+#   See test_ident for details.
+def test_global(M):
+    s = "@[reloctest]@"
+    assert s == M.str(M.symtab.reloctest, len(s))
 
 @pytest.mark.xfail(strict=True)
 #   This fails because the symbol table in the linker listing still
@@ -11,7 +19,6 @@ def M():
 #   to 0400 (as shown in the listing itself). We need to fix our
 #   symbol table loader to handle this before this will pass.
 def test_ident(M):
-    M.load('.build/obj/reloctest')
     S = M.symtab
     ident_str = "reloctest.a65"
     assert ident_str == M.str(S.ident, len(ident_str))

62a75e1a6fe861f618cf9a2d8f82b22814e4add7
Curt J. Sampson <cjs@cynic.net>
2019-09-26 16:11:32 +0900

    src/reloctest*: Add relocatable source file with ident and xfailed test
    This is a relocatable object, rather than an absolute object as with
    simple.a65. The Test script has been modified to build it, setting the
    start of the segment to 0x400. (Actually, the ident string should
    probably be in a separate data segment; we'll figure out how to do
    that later.)
    
    This fails because the symbol table in the linker listing still says
    that the object is at 0000 even though it's been relocated to 0400 (as
    shown in the listing itself). We need to fix our symbol table loader
    to handle this before this will pass.

---
+++ src/reloctest_test.py
@@ -0,0 +1,17 @@
+from    testmc.m6502 import  Machine, Registers as R, Instructions as I
+import  pytest
+
+@pytest.fixture
+def M():
+    return Machine()
+
+@pytest.mark.xfail(strict=True)
+#   This fails because the symbol table in the linker listing still
+#   says that the object is at 0000 even though it's been relocated
+#   to 0400 (as shown in the listing itself). We need to fix our
+#   symbol table loader to handle this before this will pass.
+def test_ident(M):
+    M.load('.build/obj/reloctest')
+    S = M.symtab
+    ident_str = "reloctest.a65"
+    assert ident_str == M.str(S.ident, len(ident_str))

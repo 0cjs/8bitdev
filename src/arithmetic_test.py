369248a4b1d8be7352e0603ea8ce791532a21800
Curt J. Sampson <cjs@cynic.net>
2019-11-14 21:05:45 +0900

    src/arithmetic_test: Clarify descriptions of ADC and unsigned SBC

--- src/arithmetic_test.py
+++ src/arithmetic_test.py
@@ -93,8 +93,13 @@ def test_2s_complement_abs(M):
 
 def test_adc_unsigned(M):
     ''' Unsigned ADC (add with carry).
-        We always ignore the N and V flags.
-        C flag indicates overflow; extend with $01 MSB.
+
+        The C flag set indicates overflow; to have the full result we
+        must extend the number by adding a new MSB of $01, increasing
+        the representation size by 8 bits.
+
+        We always ignore the N and V flags; an unsigned number can
+        never be negative and V is used only with signed operations.
     '''
     add(M, 0x00, 0x00); assert R(a=0x00, C=0) == M.regs    # 00 + 00 = 000
     add(M, 0xFE, 0x01); assert R(a=0xFF, C=0) == M.regs    # FE + 01 = 0FF
@@ -103,15 +108,37 @@ def test_adc_unsigned(M):
 
 def test_adc_signed(M):
     ''' Signed ADC (add with carry).
+
+        The carry of a signed addition, if it exists, ends up changing
+        the most significant bit, the sign bit. The V (overflow) flag
+        detects this. It's calculated as follows.
+
+        If the signs of the numbers are different, overflow can never
+        occur. Thus V will be clear and the result sign bit will
+        always be correct.
+
+        If the signs of the numbers are the same, the sign of the
+        result must be the same, too. If it is the same, V is clear
+        and we have a correct result.
+
+        If the sign of the result is not the same, that means we
+        overflowed and so V is set. To have the full result we must
+        extend the number (increasing the representation size by 8
+        bits) by adding a new MSB. This will be a sign-extend using
+        the _opposite_ of the N flag: if the (truncated) result has a
+        positive sign bit, we extend with $FF because it's really a
+        negative number where the sign bit "couldn't fit"; if it has a
+        negative sign bit we extend with $00 because it's really a
+        positive number.
+
         We always ignore the C flag.
-        Overflow clear: extend with N.
-        Overflow set: extend with inverted N.
     '''
 
     ##########################################################
     #   Overflow clear means that sign bit is valid.
     #   -128 <= result <= +127.
-    #   Extend with byte filled with sign bit: $00 or $FF.
+    #   No need to extend, but if we did, we'd extend with byte
+    #   filled with the sign bit: $00 or $FF.
 
     #       +00 + +00            = +000
     add(M, 0x00, 0x00); assert R(a=0x00, N=0, V=0) == M.regs
@@ -154,17 +181,34 @@ def test_adc_signed(M):
 
 def test_sbc_unsigned(M):
     ''' Unsigned SBC (subtract with carry).
-        We always ignore the N and V flags.
-        C flag clear indicates underflow (borrow used).
+
+        The C flag clear indicates underflow (i.e., the borrow was
+        used) and so our result < 0. We could generate an error,
+        or we could generate an unsigned number.
+
+        We ignore the N flag unless we want to use it to generate
+        a signed result without doing unnecessary sign-extension
+        into a new byte, increasing the size of the integer.
+
+        We always ignore the V flag.
     '''
     sub(M, 0x00, 0x00); assert R(a=0x00, C=1) == M.regs
     sub(M, 0xFF, 0x00); assert R(a=0xFF, C=1) == M.regs
     sub(M, 0xFF, 0xFD); assert R(a=0x02, C=1) == M.regs
 
     #   In these cases, C=0 (borrow used) → result < 0.
-    #   We could generate a signed result by sign-extending with $FF.
-    sub(M, 0x00, 0x01); assert R(a=0xFF, C=0) == M.regs     # $FFFF = -1
-    sub(M, 0x00, 0xFF); assert R(a=0x01, C=0) == M.regs     # $FF01 = -255
+    #   But because the result is ≥ -$80, as we can tell from the N
+    #   flag being set, we could generate a signed result by setting
+    #   bit 7 (`OR $80`).
+    sub(M, 0x00, 0x01); assert R(a=0xFF, C=0, N=1) == M.regs  # $FF = -$01
+    sub(M, 0x00, 0x80); assert R(a=0x80, C=0, N=1) == M.regs  # $80 = -$80
+
+    #   In these cases, C=0 (borrow used) → result < 0.
+    #   The result is also < $80 as we can tell by the N flag being
+    #   clear, so if we want to generate a signed result we would need
+    #   to sign-extend with an additional byte, a new MSB of $FF.
+    sub(M, 0x00, 0x81); assert R(a=0x7F, C=0, N=0) == M.regs  # $FF7F = -$81
+    sub(M, 0x00, 0xFF); assert R(a=0x01, C=0, N=0) == M.regs  # $FF01 = -$FF
 
 def test_sbc_signed(M):
     ''' Signed SBC (subtract with carry).

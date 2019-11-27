9142c96fcfe605279ed97c5e794fa83e36445841
Curt J. Sampson <cjs@cynic.net>
2019-11-27 17:32:09 +0900

    src/bigint: Rename readascdigit to convascdigit
    We remove `read` from the name because it's not reading from a buffer
    but just doing a conversion of a value in a register.
    
    Also further fixes to the comments re preserved values, etc.

--- src/bigint_test.py
+++ src/bigint_test.py
@@ -12,7 +12,7 @@ def M():
     return M
 
 ####################################################################
-#   Tests: readascdigit
+#   Tests: convascdigit
 
 @pytest.mark.parametrize('char, num', [
     ('0', 0),  ('1', 1),    ('8', 8),  ('9', 9),
@@ -20,8 +20,8 @@ def M():
     ('G',16),  ('g',16),    ('Z',35),  ('z',35),
     ('_', 40), ('\x7F', 40)
 ])
-def test_readascdigit_good(M, char, num):
-    M.call(M.symtab.readascdigit, R(a=ord(char), N=1))
+def test_convascdigit_good(M, char, num):
+    M.call(M.symtab.convascdigit, R(a=ord(char), N=1))
     assert R(a=num, N=0) == M.regs
 
 @pytest.mark.parametrize('char', [
@@ -30,11 +30,11 @@ def test_readascdigit_good(M, char, num):
     '\xAF', '\xB0', '\xB9', '\xBa',     # MSb set: '/', '0', '9', ':'
     '\xDA', '\xFa', '\xFF',             # MSb set: 'Z', 'z'
     ])
-def test_readascdigit_error(M, char):
-    M.call(M.symtab.readascdigit, R(a=ord(char), N=0))
+def test_convascdigit_error(M, char):
+    M.call(M.symtab.convascdigit, R(a=ord(char), N=0))
     assert R(N=1) == M.regs
 
-def test_readascdigit_good_exhaustive(M):
+def test_convascdigit_good_exhaustive(M):
     ''' Exhaustive test of all good values. Because we're nervous types.
         But the parametertized tests are easier for debugging errors.
     '''
@@ -42,7 +42,7 @@ def test_readascdigit_good_exhaustive(M):
     def ordrange(a, z):
         return range(ord(a), ord(z)+1)
     def readasc(a):
-        M.call(M.symtab.readascdigit, R(a=a, N=1))
+        M.call(M.symtab.convascdigit, R(a=a, N=1))
         return M.regs
 
     for num,char in zip(count(0), ordrange('0','9')):
@@ -52,7 +52,7 @@ def test_readascdigit_good_exhaustive(M):
     for num,char in zip(count(10), ordrange('a', '\x7F')):
         assert R(a=num, N=0) == readasc(char), '{} ??? char {}'.format(num, char)
 
-def test_readascdigit_error_exhaustive(M):
+def test_convascdigit_error_exhaustive(M):
     ''' Exhaustive test of all bad values. Because we're nervous types.
         But the parametertized tests are easier for debugging errors.
     '''
@@ -63,7 +63,7 @@ def test_readascdigit_error_exhaustive(M):
         range(ord('\x7F')+1,  255 ),
         )
     for char in badchars:
-        M.call(M.symtab.readascdigit, R(a=char, N=0))
+        M.call(M.symtab.convascdigit, R(a=char, N=0))
         assert R(N=1) == M.regs, 'char {} should be bad'.format(char)
 
 ####################################################################

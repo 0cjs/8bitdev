06081bca17cf3385c76178cf2debe0f0c5edf034
Curt J. Sampson <cjs@cynic.net>
2019-12-30 16:19:14 +0900

    src/simple-asl_test: Add test for fill routine

--- src/simple-asl_test.py
+++ src/simple-asl_test.py
@@ -24,3 +24,17 @@ def test_addxy(M):
     expected = 0x2A + 0x33
     assert expected == M.byte(S.xybuf)
     assert R(a=expected, x=0x2A, y=0x33) == M.regs
+
+@pytest.mark.parametrize('len', (1, 2, 16, 255))    # len must be at least 1
+def test_fill(M, len):
+    S = M.symtab
+    base = 0x1234       # Fill does not include base address
+
+    guard = [0xFF] * 2
+    M.deposit(base-2, [0xFF]*260)
+
+    #   There's also a temporary symbol in here but we currently have
+    #   neither a reliable way to access it or any good use for it.
+    M.depword(S.fillbase, base)
+    M.call(S.fill, R(y=len))
+    assert guard + [0xFF] + [0]*len + guard == M.bytes(base-2, len+5)

7763917f9c4b709819ddbd4c82bca78e591148f7
Curt J. Sampson <cjs@cynic.net>
2020-06-22 13:36:15 +0900

    src/simple-a68: Create mc6800.Machine and load object code into it
    This changes our test framework to parametrize the Machine type; rather
    than always using a testmc.m6502.Machine, we now use whatever Machine
    has been imported by the caller, so we use whatever Machine the tests
    expect. Cleverly, this requires no changes to the tests themselves.
    
    The M, S and R fixtures really should be moved to the testmc module
    at some point, since they're generic to anything using it.
    
    We test that the correct data are being loaded into a 6800 Machine and that
    we can step an LDAA instruction (the only one that's implemented); further
    tests for call() will have to wait until we have an RTS instruction and
    call() is implemented.

--- src/conftest.py
+++ src/conftest.py
@@ -1,12 +1,17 @@
-from    testmc.m6502 import  Machine
 import  pytest
 
 @pytest.fixture
 def M(request):
-    ''' A simulated machine with the object file loaded. The calling
-        module should define a global `object_file` bound to a string
-        giving the path to the object file relative to ``.build/obj/``.
+    ''' A simulated machine with the object file loaded.
+
+        The caller must have two global variables defined in its
+        module:
+        - `Machine`: The class of the CPU/machine simulator to instantiate.
+          Normally this is simply imported from `testmc`.
+        - `object_file`: A `str` giving the path to the object file to load
+          into the machine, relative to ``.build/obj/``.
     '''
+    Machine = getattr(request.module, 'Machine')
     M = Machine()
     #   XXX This is probably not the best way to find this file; it makes
     #   this dependent on the CWD being the project root dir above .build/.

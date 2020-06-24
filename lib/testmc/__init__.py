639cc9678bfd39e6b1e89f901799f5be46d06435
Curt J. Sampson <cjs@cynic.net>
2020-06-24 16:09:29 +0900

    testmc tmc_tid(): Help generate nicer pytest test IDs
    This can be used in as `pytest.mark.parametrized(..., ids=tmc_tid)` to
    generate IDs with numbers in hex and strings more distinctly separated from
    numbers.
    
    Note that this cannot be called testmc_tid() because pytest will discover
    it and try to run it as a test!

--- lib/testmc/__init__.py
+++ lib/testmc/__init__.py
@@ -1,2 +1,24 @@
 ''' testmc -- test framework for microcomputer systems
 '''
+
+from    numbers  import Number
+
+__all__ = ['hexid']
+
+def tmc_tid(x):
+    ''' `testmc` pytest ID. This helps generate nicer test IDs for pytest
+        parametrized tests, via ``parametrize(..., ids=testmc_tid)``, for
+        the typical parameter data used in tests using `testmc`.
+
+        - If `x` is a `Number` and ≥ 2, return a hex representation
+          prefixed with ``$``.
+        - If `x` is a `str`, return its `repr` to put quotes around it.
+          (This helps avoid confusion between hex digits and letters.)
+        - Otherwise return `x`.
+    '''
+    if isinstance(x, Number) and x >= 2:
+        return '${:02X}'.format(x)
+    elif isinstance(x, str):
+        return repr(x)
+    else:
+        return x

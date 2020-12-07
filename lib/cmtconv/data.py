39d0bcc42b0ee37b97cfa95d49124a6a66f46d65
Curt J. Sampson <cjs@cynic.net>
2020-12-07 14:44:43 +0900

    cmtconv.data: BlockHeader and Block classes extracted from cmtconv.jr200
    This is the first step in starting to clarify the layering of the code
    (files/blocks, byte streams, bit streams and audio data). We choose a more
    generic name for the module with the idea that when we add the next
    platform the current module will give us the API and we'll be able to see
    what parts need to be separated out into platform-specific modules.
    
    We extract this small bit first with the aim of adding unit tests and
    bringing all the block handling together. (Right now it appears that the
    FileHeader is a sort of block, but is completely separate from the
    BlockHeader, and various other stand-alone functions such as
    checksums_valid are actually block-related.)
    
    The one debug() call in this code has been replaced with a call to
    logging.debug() which currently produces no output. In the future we should
    add a command line option to set the level; in the meantime it can be
    turned on by temporarily adding the following to an appropriate place in
    the code:
    
        logging.getLogger().setLevel(logging.DEBUG)

---
+++ lib/cmtconv/data.py
@@ -0,0 +1,117 @@
+''' cmtconv.data: byte data stored in tape blocks and files
+
+    This is above the level of both the bitstream representation of the
+    bytes (mark/space sequences) and the audio data representing the
+    bitstream.
+
+    XXX This is currently all specific to the JR-200 format. When we add a
+    second format we'll probably want to make these classes define the API
+    and hold common operations (if any), and use subclasses for particular
+    implementations.
+'''
+
+from    enum  import Enum
+from    logging  import debug
+
+####################################################################
+#   Tape Blocks
+
+class BlockHeader(object):
+    '''Interprets the block header'''
+
+    def __init__(self, header, block_number, length, addr, raw_bytes):
+        '''Caller is responsible for ensuring bytes match interpretation'''
+        self.header         = header
+        self.block_number   = block_number
+        self.length         = length
+        self.addr           = addr
+        self.raw_bytes      = raw_bytes
+
+    @staticmethod
+    def from_bytes(raw_bytes):
+        '''Construct the block header from raw bytes'''
+        b = raw_bytes
+        header = (b[0], b[1])
+        if header != (2, 42):
+            raise ValueError('Invalid signature in header: {}'.format(header))
+
+        length = b[3]
+        if length == 0:
+            length = 256
+        addr = b[4] * 256 + b[5]
+
+        return BlockHeader(header, b[2], length, addr, raw_bytes)
+
+    @staticmethod
+    def make(block_number, length, addr):
+        '''Build a block header from details'''
+        if block_number < 0 or block_number > 255:
+            raise ValueError('Block number must be in range 0-255: {}' %
+                block_number)
+        if length < 0 or length > 256:
+            raise ValueError('Length must be in range 0-256: {}' % length)
+        if addr < 0 or addr > 0xffff:
+            raise ValueError('Address must be in range 0x0000-0xffff: {}' %
+                addr)
+        header = (2, 42)
+        b = bytearray()
+        b.extend(header)
+        b.append(block_number)
+        b.append(0 if length == 256 else length)
+        b.append(addr // 256)
+        b.append(addr % 256)
+        return BlockHeader(header, block_number, length, addr, b)
+
+    @staticmethod
+    def make_tail():
+        '''Make a tail block'''
+        return BlockHeader.make(255, 255, 0)
+
+    def __str__(self):
+        s = ''
+        s += 'JR-200 Data block header\n'
+        s += 'Header: {}\n'.format(tuple(hex(x) for x in self.header))
+        s += 'Block: {}\n'.format(self.block_number)
+        s += 'Length: {}\n'.format(self.length)
+        s += 'Address: {}\n'.format(self.addr)
+        return s
+
+    def is_tail(self):
+        #FIXME: what happens when we try to save the whole 64k?
+        #Is the block number alone enough?
+        return self.block_number == 255 and self.length == 255
+
+    def calc_checksum(self):
+        if self.is_tail():
+            return 0
+        else:
+            return sum(self.raw_bytes) % 256
+
+class Block(object):
+    '''Represents a block of data'''
+
+    def __init__(self, header, data, checksum):
+        self.header     = header
+        self.data       = data
+        self.checksum   = checksum
+
+    def __str__(self):
+        s = ''
+        s += str(self.header)
+        for (i, x) in enumerate(self.data):
+            if i % 20 == 0:
+                s+= '\n'
+            s += '  {0:02x}'.format(x)
+        s += '\n\nChecksum: %x' % self.checksum
+        return s
+
+    def calc_checksum(self):
+        if self.header.is_tail():
+            return 0
+        else:
+            header_chksum = self.header.calc_checksum()
+            data_chksum = sum(self.data) % 256
+            chksum = (header_chksum + data_chksum) % 256
+            debug('header_chksum: %d, data_chksum: %d, chksum: %d' %
+                  (header_chksum,data_chksum, chksum))
+            return chksum

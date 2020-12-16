494f483853fc808f35b03528beb7a23c3bca9024
Curt J. Sampson <cjs@cynic.net>
2020-12-16 15:37:51 +0900

    testmc: Move LSB(),MSB() up to top module
    These are generic operations on Python values.
    Note that they do not imply anything about endianness.

--- lib/testmc/mc6800/__init__.py
+++ lib/testmc/mc6800/__init__.py
@@ -4,8 +4,7 @@
     simulation.
 '''
 
-from    testmc.mc6800.opimpl   import LSB, MSB
 from    testmc.mc6800.machine  import Machine
 from    testmc.mc6800.opcodes  import Instructions
 
-__all__ = ['LSB', 'MSB', 'Machine', 'Instructions']
+__all__ = ['Machine', 'Instructions']

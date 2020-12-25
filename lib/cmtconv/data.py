df0f98bced6e2c3ab20944fefbe80c1cdb694151
Curt J. Sampson <cjs@cynic.net>
2020-12-25 17:23:29 +0900

    cmtconv.data.FileHeader: New class based on Block to replace old one

--- lib/cmtconv/data.py
+++ lib/cmtconv/data.py
@@ -21,20 +21,21 @@
 
 '''
 
+from enum import IntEnum
+
 ####################################################################
 #   Tape Blocks
 
 class Block(object):
-    ''' JR-200 Tape Block
-
-        0-1: magic number $02 $2A (2,42)
-          2: block number: 0-254; 255 indicates end block
-          3: length of the data portion of the block; 0=256 bytes
-             (255 for tail block)
-        4-5: load address ($0000 if not used)
-        6…n: data (datalen bytes)
-        n+1: checksum: sum of bytes 0…n modulo 256
-    '''
+    ' A JR-200 Tape Block '
+    # Block format on tape:
+    #   0-1: magic number $02 $2A (2,42)
+    #     2: block number: 0-254; 255 indicates end block
+    #     3: length of the data portion of the block; 0=256 bytes
+    #        (255 for tail block)
+    #   4-5: load address ($0000 if not used)
+    #   6…n: data (datalen bytes)
+    #   n+1: checksum: sum of bytes 0…n modulo 256
 
     @classmethod
     def make(cls, blockno, addr, data=b'', checksum=None):
@@ -176,3 +177,114 @@ class TailBlock(Block):
     def __repr__(self):
         return '{}.{}(addr={})'.format(
             self.__class__.__module__, self.__class__.__name__, hex(self.addr))
+
+class FileHeader(Block):
+    ''' The header block for a JR-200 tape file. This is a standard block
+        with blockno 0, address $FFFF and datalen 26, and the file metadata
+        in the data section. It is always a 600 baud block.
+
+        This is an immutable object because it has so few attributes
+        that it's easy to create a new one if something need be changed.
+    '''
+    # Data section contents:
+    #    0-15: file name
+    #      16: BASIC(0)/Binary(1)
+    #      17: Baud rate of following blocks: 0=2400, 1=600
+    #   18-25: pad: $FF
+
+    @classmethod
+    def make(cls, filename, filetype, baudrate):
+        if len(filename) > 16:
+            raise ValueError(
+                'len(filename) must be < 16 (len={})'.format(len(filename)))
+        if filetype not in cls.FileType:
+            raise ValueError('filetype must be BASIC or BINARY, not {}'
+                .format(repr(filetype)))
+        if baudrate not in cls.BaudRate:
+            raise ValueError('baudrate must be B_2400 or B_600, not {}'
+                .format(repr(baudrate)))
+
+        fnfill = bytearray(16)
+        fnfill[0:len(filename)] = filename
+        return cls.from_bytes(
+            bytes([
+                0x02, 0x2A,             # magic number
+                   0, 0x1A,             # block 0, datalen
+                0xFF, 0xFF,             # address: $FFFF
+            ])
+            + fnfill                    # filename
+            + bytes([filetype, baudrate])
+            + bytes([0xFF]*8)           # padding
+            , checksum=False)
+
+    #   The length of the entire block, including checksum.
+    blocklen = Block.headerlen + 26 + 1
+
+    @classmethod
+    def from_bytes(cls, blockbytes, checksum=True):
+        ''' Create a `FileHeader` from exactly `blocklen` bytes, which must
+            be the entire contents of a block read from tape. A
+            `ValueError` or `ChecksumError` will be raised if the block is
+            not a valid FileHeader block.
+
+            If `checksum` is `False`, `blockbytes` must not have a trailing
+            checksum byte (i.e., length `blocklen` - 1).
+        '''
+        if not checksum:
+            blockbytes += b'\xEE'   # dummy checksum that will be ignored
+        if len(blockbytes) != cls.blocklen:
+            raise ValueError('bad length: expected={} actual={}'
+                .format(cls.blocklen, len(blockbytes)))
+        block = FileHeader(blockbytes[Block.headerlen:-1])
+
+        if block.blockno != 0:
+            raise ValueError('bad blockno: expected=${:02X} actual=${:02X}'
+                .format(0, block.blockno))
+        if block.addr != 0xFFFF:
+            raise ValueError('bad addr: expected=${:04X} actual=${:04X}'
+                .format(0xFFFF, block.addr))
+        if checksum and block.checksum() != blockbytes[-1]:
+            raise cls.ChecksumError('expected=${:02X} actual=${:02X}'
+                .format(block.checksum(), blockbytes[-1]))
+
+        return block
+
+    def __init__(self, data):
+        ' `data` must be already validated. '
+        super().__init__(0, 0xFFFF, data)
+
+    @property
+    def filename(self):
+        ''' The filename is a 0-15 character `bytes` using JR-200 charset.
+
+            This may include Japanese characters, and so should probably be
+            a (Unicode) Python `str`, but we don't do character set
+            conversion right now.
+        '''
+        return self.data[0:16].rstrip(b'\x00')
+
+    class FileType(IntEnum): BASIC = 0; BINARY = 1
+    BASIC  = FileType.BASIC
+    BINARY = FileType.BINARY
+
+    @property
+    def filetype(self):
+        ' Returns either `BASIC` or `BINARY`. '
+        return self.FileType(self.data[16])
+
+    class BaudRate(IntEnum): B_2400 = 0; B_600 = 1
+    B_2400 = BaudRate.B_2400
+    B_600  = BaudRate.B_600
+
+    @property
+    def baudrate(self):
+        ''' Returns the baud rate used by the remaining blocks in the file,
+            `B_2400` or `B_600`. The FileHeader block (this block) is
+            always stored at 600 baud.
+        '''
+        return self.BaudRate(self.data[17])
+
+    def __repr__(self):
+        return '{}.{}(filename={}, filetype={} baudrate={})'.format(
+            self.__class__.__module__, self.__class__.__name__,
+            self.filename, self.filetype.name, self.baudrate.name)

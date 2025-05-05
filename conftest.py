ca0aaaadb5c9bea643c7ff172c3807414e4a0ca8
Curt J. Sampson <cjs@cynic.net>
2025-05-06 00:36:49 +0900

    conftest.py: Explain why this is at the top level

--- conftest.py
+++ conftest.py
@@ -1,2 +1,5 @@
+#   This must be at the top level of 8bidev so that
+#   it will find the tests in the t8dev submodule.
+
 from    pytest_pt  import *     # Plugin to find/execute .pt files as tests
 from    testmc.pytest  import * # t8dev test framework fixtures

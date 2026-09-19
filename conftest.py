4c7b07058e7e7f09e647ba023bc0772761b8d4da
Curt J. Sampson <cjs@cynic.net>
2026-09-19 12:50:12 +0900

    conftest.py: Remove pytest_pt import to stop duplicate test collection
    Per the commit in t8dev bringing it to 0.4.0.dev9, new versions of
    pytest_pt, including all versions allowed now by t8dev, autoload
    themselves. So our import in conftest.py was now loading a second
    copy of the hook, causing every .pt file to be collected twice.

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
-#   This must be at the top level of 8bidev so that
-#   it will find the tests in the t8dev submodule.
+#   This currently doesn't need to be at the top level: only tests under
+#   src/ use this fixture. But on the other hand, it can be nice to
+#   keep all conftest.py stuff at the top level for easy reference.
 
-from    pytest_pt  import *     # Plugin to find/execute .pt files as tests
 from    testmc.pytest  import * # t8dev test framework fixtures
